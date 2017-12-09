b6fd23d0224a298fb1158a2aa2a141a7d8415c48
bnmghjtyu0 <bnmghjtyu0@gmail.com>
2017-12-09 18:21:54 +0800

    第五步:建立陣列，加入for迴圈

--- src/components/hello2.vue
+++ src/components/hello2.vue
@@ -6,21 +6,13 @@
   <button class="btn btn-primary">Go</button>
   </span>
 </div>
-    <div class="media">
+    <div class="media" v-for="item in messages">
       <img class="align-self-start mr-3" src="http://lorempixel.com/100/100/food/1/" alt="Generic placeholder image">
       <div class="media-body">
-        <h5 class="mt-0">Top-aligned media</h5>
-        <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
-        <p>Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+        <h5 class="mt-0">{{item.username}}</h5>
+        <p>{{item.message}}</p>
       </div>
     </div>
-    <div class="media">
-      <div class="media-body">
-        <h5 class="mt-0 mb-1">Media object</h5>
-        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-      </div>
-      <img class="ml-3" src="http://lorempixel.com/100/100/food/3/" alt="Generic placeholder image">
-      </div>
     </div>
 </template>
 
@@ -29,7 +21,17 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      messages: [
+        {
+          username:'Ricahrd',
+          message :'Hello Winni'
+        },
+        {
+          username:'Winni',
+          message :'安安'
+        }
+      ]
     }
   }
 }
